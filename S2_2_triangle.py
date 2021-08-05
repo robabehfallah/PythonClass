faaac8b61aadedb5ddd80d88ba5b13fbe2bfe943
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-05 12:23:06 +0430

    Update S2_2_triangle.py

--- S2_2_triangle.py
+++ S2_2_triangle.py
@@ -1,6 +1,6 @@
-a = int(input("enter first number? \n"))
-b = int(input("enter Second number? \n"))
-c = int(input("enter third number? \n"))
+a = int(input("enter first side's length? \n"))
+b = int(input("enter Second side's length? \n"))
+c = int(input("enter third side's length? \n"))
 
 if (a+b > c) and (a+c > b) and (b+c > a):
     print("they can form a triangle")
