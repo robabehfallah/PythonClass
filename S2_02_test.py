24a325c395906e3afdc4753b4f761fd0c84849e0
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-05 12:50:13 +0430

    Create S2_02_test.py

---
+++ S2_02_test.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age? \n"))
+name = input("enter your name? \n")
+
+if (age > 30 and name == "Ali") or (age < 20 and name != "Ali"):
+    print("You can inter")
+else:
+    print("You can not inter")
