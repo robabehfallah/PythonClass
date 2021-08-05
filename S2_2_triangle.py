ac119f88aae03b59cd256d78ac55d7e056ee724c
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-05 12:49:32 +0430

    Delete S2_2_triangle.py

--- S2_2_triangle.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter first side's length? \n"))
-b = int(input("enter Second side's length? \n"))
-c = int(input("enter third side's length? \n"))
-
-if (a+b > c) and (a+c > b) and (b+c > a):
-    print("they can form a triangle")
-else:
-    print("they can not form a triangle")
