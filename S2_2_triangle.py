b1b0893e30aba11819f2d8bec27167efa76b99a0
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-05 12:22:19 +0430

    Update S2_2_triangle.py

--- S2_2_triangle.py
+++ S2_2_triangle.py
@@ -2,7 +2,7 @@ a = int(input("enter first number? \n"))
 b = int(input("enter Second number? \n"))
 c = int(input("enter third number? \n"))
 
-if (a+b > c) or (a+c > b) or (b+c > a):
+if (a+b > c) and (a+c > b) and (b+c > a):
     print("they can form a triangle")
 else:
     print("they can not form a triangle")
