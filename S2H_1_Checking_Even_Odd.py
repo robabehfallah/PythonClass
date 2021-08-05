1f77c3c3ab84d2d2533b742078e51b54952323f9
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-05 13:08:15 +0430

    Update S2H_1_Checking_Even_Odd.py

--- S2H_1_Checking_Even_Odd.py
+++ S2H_1_Checking_Even_Odd.py
@@ -1,1 +1,8 @@
+#A simple Code to check if a number is Odd or Even
+Number = int(input("Please enter your integer Number= \n"))
+
+if (Number%2 == 0):
+    print("You entered",Number, "Which an Even Number")
+else:
+    print("You entered",Number, "Which an Odd Number")
 
