262e4e4bc0eb5a2b6194a7ae5acaed3f10dd7f2b
Farhad Ebrahimzadeh <farhad.eg87@gmail.com>
2021-08-26 11:25:35 +0430

    Create S4_.py

---
+++ S4_.py
@@ -0,0 +1,6 @@
+list_old = ["Ali" , "Reza", "Mohsen", "Farhad", "Neda", "mokhi"]
+list_new = ['Farhad', 'Neda', 'Saeed']
+list_final = list_old + list_new
+print(list_final)
+print(len(set(list_final)), 'Unique Names')
+print(set(list_final))
