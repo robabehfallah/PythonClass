29b362a6100029efad8666c8f2a2b1910dc66769
robabehfallah <87467396+robabehfallah@users.noreply.github.com>
2021-09-08 23:32:34 +0430

    Update S05-E01.py

--- S05-E01.py
+++ S05-E01.py
@@ -1,1 +1,11 @@
+X1 = int(input("Please Enter a numbre:"))
+X2 = int(input("Please Enter a numbre:"))
+
+if X1 < X2:
+    for x in range(X1,X2):
+        print(x)
+elif X1 > X2:
+    for x in range(X2,X1):
+            print(x)
+else: print(X1)
 
